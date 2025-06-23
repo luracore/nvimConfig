1ba725814b64b8bf1df22119816da0c9d712adfb
lucca <luccarcr@gmail.com>
2025-06-23 15:32:03 -0300

    Melhorias nos atalhos

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -19,6 +19,17 @@ vim.keymap.set({"n", "v"}, "<leader>y", '"+y', {desc = "Copia para o teclado"})
 vim.keymap.set({"n", "v"}, "<leader>d", '"+d', {desc = "Copia para o teclado e deleta"})
 vim.keymap.set({"n", "v"}, "<leader>p", '"+p', {desc = "Cola a cópia do teclado"})
 
--- Atalhos para git
-vim.keymap.set("n", "<leader>ga", ":!git add .<CR>", {desc = "Git add ." })
-vim.keymap.set("n", "<leader>gc", ":terminal git commit<CR>", {desc = "Git commit interativo" })
+-- Atalhos para criar e movimentar entre abas
+vim.keymap.set("n", "<leader><Tab>", ":tabnew<CR>", {desc = "Cria aba"})
+vim.keymap.set("n", "<Tab>", "gt", {desc = "Vai para a próxima aba"})
+vim.keymap.set("n", "<S-Tab>", "gT", {desc = "Vai para a aba anterior"})
+
+-- Atalhos para criar e movimentar entre partições de tela
+vim.keymap.set("n", "<leader>sq", ":split<CR>", {desc = "Separa a tela horizontalmente"})
+vim.keymap.set("n", "<leader>sw", ":vsplit<CR>", {desc = "Separa a tela verticalmente"})
+vim.keymap.set("n", "<leader>sh", "<C-w>h", {desc = "Separa a tela horizontalmente"})
+vim.keymap.set("n", "<leader>sl", "<C-w>l", {desc = "Separa a tela horizontalmente"})
+vim.keymap.set("n", "<leader>sk", "<C-w>k", {desc = "Separa a tela horizontalmente"})
+vim.keymap.set("n", "<leader>sj", "<C-w>j", {desc = "Separa a tela horizontalmente"})
+
+vim.keymap.set("n", "<leader>t", ":terminal<CR>", {desc = "Abre terminal"})
